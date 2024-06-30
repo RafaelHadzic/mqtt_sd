a92e4da722fbe6e03b8d95d62528b452a8ba05e5
Guilherme Thiesen <guilherme.thiesen@outlook.com>
2024-06-30 16:52:46 -0300

    feat: calculo de tempo por rua

--- subscribe.ts
+++ subscribe.ts
@@ -18,4 +18,24 @@ client.on('connect', () => {
 })
 client.on('message', (topic: string, payload: { toString: () => any }) => {
     console.log(payload.toString())
+    
+    const message = JSON.parse(payload.toString())
+    
+    const tempoRua1 = message[0]['street1'] * 4 + 10
+    const tempoRua2 = message[0]['street2'] * 4 + 10
+
+    console.log(`Tempo de espera na rua 1: ${tempoRua1} segundos`)
+    console.log(`Tempo de espera na rua 2: ${tempoRua2} segundos`)
+
+    const newMessage = JSON.stringify([{
+        "time": `${new Date().toLocaleString()}`,
+        "streetTime1": `${tempoRua1}`,
+        "streetTime2": `${tempoRua2}`,
+    }])
+    client.publish('/semaforo/time', newMessage, { qos: 0, retain: false }, (error) => {
+        if (error) {
+            console.error(error)
+        }
+    })
+    console.log(`Publish to topic '/semaforo/time'`)
 })
